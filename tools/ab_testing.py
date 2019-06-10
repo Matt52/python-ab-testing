d3c5152e1b8e2f28d9e6494d56d4d5d6105935a2
Matúš Baniar <matus.baniar@gmail.com>
2019-06-10 22:40:33 +0200

    z test development

--- tools/ab_testing.py
+++ tools/ab_testing.py
@@ -2,28 +2,75 @@ import numpy as np
 from scipy.stats import norm
 
 
-def AB_test(variants, totals, successes, confidance = 0.95, sides = 2):
+def z_test_power(mu_A, mu_B, se_A, se_B, alpha = 0.05, sides = 2):
     
-    nA = totals[0]
-    cA = successes[0]
-    nB = totals[1]
-    cB = successes[1]
+    if mu_A <= mu_B:
+        power = 1 - norm.cdf(norm.ppf(1 - (alpha / sides)) - (mu_B - mu_A)/np.sqrt(se_A**2 + se_B**2))
+    else:
+        power = 1 - norm.cdf(norm.ppf(1 - (alpha / sides)) - (mu_A - mu_B)/np.sqrt(se_A**2 + se_B**2))
+
+    return power
+
+#version used in abtestguide and surveymoneky calculators
+def z_test_power_alternative(mu_A, mu_B, se_A, se_B, alpha = 0.05, sides = 2):
+    if mu_A <= mu_B:
+        x = mu_A + norm.ppf(1 - (alpha / sides)) * se_A 
+        power = 1 - norm.cdf((x - mu_B) / se_B)
+    else:
+        x = mu_B + norm.ppf(1 - (alpha / sides)) * se_B
+        power = 1 - norm.cdf((x - mu_A) / se_A)
+
+    return power
+
+
+def z_test_sample_size(conv_r, mde, confidance = 0.95, power = 0.8, test_share_size = 0.5):
     alpha = 1 - confidance
-    mu_A = cA / nA
-    mu_B = cB / nB
+    conv_exp = conv_r * (1 + mde)
+    var = conv_r * (1 - conv_r) + conv_exp * (1 - conv_exp)
+    
+    size = (var / ((conv_exp - conv_r)**2)) * (norm.ppf(power) + norm.ppf(1 - alpha/2))**2
+
+    share_ratio = (1 - test_share_size) / test_share_size
+    size_adj = (2*size*((1 + share_ratio)**2)) / (4*share_ratio)
+
+    return [(size_adj) / (1 + share_ratio), (size_adj*share_ratio) / (1 + share_ratio)]
+
+
+#version that should be used in abtestguide to be consistent with their power calculation
+def z_test_sample_size_alternative(conv_r, mde, confidance = 0.95, power = 0.8):
+    alpha = 1 - confidance
+    conv_exp = conv_r * (mde + 1)
+
+    size = ((norm.ppf(1 - alpha/2) * np.sqrt(conv_r * (1 - conv_r)) + norm.ppf(power) * np.sqrt(conv_exp * (1 - conv_exp))) / (conv_exp - conv_r))**2
+
+    return size
+
+
+def AB_z_test(totals_A, successes_A, totals_B, successes_B, confidance = 0.95, test_type = 'two-tailed'):
+    
+    if test_type not in ['one-tailed', 'two-tailed']:
+        raise ValueError("Invalid test type. Expected one of: %s" % ['one-tailed', 'two-tailed'])
+    elif test_type == 'one-tailed':
+        sides = 1
+    else:
+        sides = 2
+
+    alpha = 1 - confidance
+    mu_A = successes_A / totals_A
+    mu_B = successes_B / totals_B
     
     if mu_A > mu_B:
-        winning = variants[0]
+        winning = 'A'
     elif mu_A < mu_B:
-        winning = variants[1]
+        winning = 'B'
     else:
         winning = 'None'
     
     uplift = (mu_B - mu_A) / mu_A
     var_A = mu_A * (1 - mu_A)
     var_B = mu_B * (1 - mu_B)
-    se_A = np.sqrt(var_A / nA)
-    se_B = np.sqrt(var_B / nB)
+    se_A = np.sqrt(var_A / totals_A)
+    se_B = np.sqrt(var_B / totals_B)
     
     Z = (mu_B - mu_A) / np.sqrt(se_A**2 + se_B**2)
     pvalue = sides * (1 - norm.cdf(abs(Z)))
@@ -33,26 +80,22 @@ def AB_test(variants, totals, successes, confidance = 0.95, sides = 2):
     else:
         result = 'not significant'
     
-    if mu_A <= mu_B:
-        x = mu_A + norm.ppf(1 - (alpha / sides)) * se_A #i.e. ppf = 1.96 for alpha = 0.05 and sides = 2
-        power = 1 - norm.cdf((x - mu_B) / se_B)
-    else:
-        x = mu_B + norm.ppf(1 - (alpha / sides)) * se_B
-        power = 1 - norm.cdf((x - mu_A) / se_A)
+    power = z_test_power(mu_A, mu_B, se_A, se_B, alpha, sides)
+    #power = z_test_power_alternative(mu_A, mu_B, se_A, se_B, alpha, sides)
         
     
-    print("""winning: {0} ({1} - confidance: {10})
+    print("""winning: {0} ({1})
 conversion rate A: {2}%
 conversion rate B: {3}%
 uplift: {4}%
-se A: {5}
-se B: {6}
+standard error A: {5}
+standard error B: {6}
 Z-score: {7}
 p-value: {8}
-power: {9}""".format(winning, result, round(mu_A*100,3), round(mu_B*100,3), round(uplift*100,3), round(se_A,5), round(se_B,5), round(Z,5), round(pvalue,5), round(power,5), confidance)
+power: {9}""".format(winning, result, round(mu_A*100,3), round(mu_B*100,3), round(uplift*100,3), round(se_A,5), round(se_B,5), round(Z,5), round(pvalue,5), round(power,5))
          )
 
-#########################################################
+##################################################################################################################
 
 def pbb_conversion(totals, successes, sim_count = 10000):
     beta_samples = np.array([np.random.beta(successes[i] + 1/2, totals[i] - successes[i] + 1/2, sim_count) for i in range(len(totals))])
